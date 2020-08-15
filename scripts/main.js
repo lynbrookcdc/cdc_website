dd3b09a929e768da0eb446341513eb1d55e8ef61
David Choi <davidchoi@MacBook-Pro.local>
2020-08-15 10:31:53 -0700

    safety upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,75 +1,38 @@
 const submitButton = document.getElementById("submit_button");
 submitButton.onclick = () => window.open('https://forms.gle/5y9HjxX23axi9nrU8', '_blank');
 
-console.log("hi");
-//TODO ADD TITLE
+
+var submissionArr = [];  //TODO change submissionsArr to a map (maybe)
+var categoriesArr = new Map();
 
 //JSON link: https://spreadsheets.google.com/feeds/cells/1fDiPcoEeUrMZASy5Gtp_uDZ9hDa7J9GVyzHRskv_tm4/1/public/full?alt=json
 const url = "https://spreadsheets.google.com/feeds/cells/1fDiPcoEeUrMZASy5Gtp_uDZ9hDa7J9GVyzHRskv_tm4/1/public/full?alt=json"
 fetch(url).then(res => res.json())
     .then((data) => {
-        loadImages(data)
+        organiseData(data);
+        loadImages(submissionArr, categoriesArr)
+
     })
     .catch(err => {
         throw err
     });
 
 
-let categories = new Map();
 
 
-function loadImages(data) {
-    const recentGallery = document.getElementById("recent_gallery");
-    let submissions = organiseData(data);
 
 
-    for (let i = submissions.length - 1; i > submissions.length - 8 && i >= 0; i--) {
-        let submission = submissions[i]
-        let img = document.createElement('img');
-        img.src = submission.image;
-        img.classList.add("container-fluid")
-        img.classList.add("image")
-        recentGallery.appendChild(img);
-    }
-
-
-    const template = document.getElementById("gallery_template");
-
-    //TODO use categories entry iterator
-    let catIter = categories.entries();
-    let arr = Array.from(catIter);
-    arr.reverse().forEach(category => {
-
-        let clone = template.content.cloneNode(true);
-
-
-        let title = clone.querySelector("h5");
-        let imageHolder = clone.querySelector(".gallery");// is an array
-        console.log(category);
-        title.innerText = category[0];
-        for (let i = category[1].length - 1; i > category[1].length - 8 && i >= 0; i--) {
-            let img = document.createElement("img")
-            img.src = category[1][i].image;
-            img.classList.add("container-fluid")
-            img.classList.add("image")
-            console.log(category[1][i]);
-            imageHolder.appendChild(img);
-        }
-        document.getElementById("galleries").appendChild(clone);
-    })
-
-}
 
 
 function organiseData(data) {
+
+
     console.log(data)
     let cellArr = data.feed.entry;
 
 
     let maxRow = Number(data.feed.gs$rowCount.$t);
     let maxCol = 7;
-    let submissionArr = [];
-
     let rCount = 0;
 
 
@@ -84,6 +47,7 @@ function organiseData(data) {
         if (submissionArr[rCount] == null) {
             submissionArr[rCount] = new submission();
         }
+
 
         switch (Number(cell.gs$cell.col)) {
             case 1:
@@ -100,21 +64,30 @@ function organiseData(data) {
                 }
                 break;
             case 5:
-                if (!categories.has(cell.gs$cell.$t)) {
-                    categories.set(cell.gs$cell.$t, []);
+                if (!categoriesArr.has(cell.gs$cell.$t)) {
+                    categoriesArr.set(cell.gs$cell.$t, []);
                 }
                 submissionArr[rCount].category = cell.gs$cell.$t;
                 break;
+
             case 7:
+                submissionArr[rCount].title = cell.gs$cell.$t;
+                break;
+            case 8:
                 submissionArr[rCount].image = cell.gs$cell.$t;
+                break;
+            case 9:
+                submissionArr[rCount].id = cell.gs$cell.$t;
                 break;
         }
     }
 
-    if (categories.size > 0) {
+    if (categoriesArr.size > 0) {
 
         submissionArr.forEach(submission => {
-                categories.get(submission.category).push(submission);
+                if (categoriesArr.has(submission.category)) {
+                    categoriesArr.get(submission.category).push(submission);
+                }
             }
         )
     }
@@ -124,13 +97,56 @@ function organiseData(data) {
 }
 
 
+
+function loadImages(submissions, categoriesArr) {
+    const recentGallery = document.getElementById("recent_gallery");
+
+
+    for (let i = submissions.length - 1; i > submissions.length - 8 && i >= 0; i--) {
+        let submission = submissions[i]
+        let img = document.createElement('img');
+        img.src = submission.image;
+        img.classList.add("container-fluid")
+        img.classList.add("image")
+        recentGallery.appendChild(img);
+    }
+
+
+    const template = document.getElementById("gallery_template");
+
+    //TODO use categories entry iterator
+    let catIter = categoriesArr.entries();
+    let arr = Array.from(catIter);
+    arr.reverse().forEach(category => {
+
+        let clone = template.content.cloneNode(true);
+
+
+        let title = clone.querySelector("h5");
+        let imageHolder = clone.querySelector(".gallery");// is an array
+        title.innerText = category[0];
+        for (let i = category[1].length - 1; i > category[1].length - 8 && i >= 0; i--) {
+            let img = document.createElement("img")
+            img.src = category[1][i].image;
+            img.classList.add("container-fluid")
+            img.classList.add("image")
+            imageHolder.appendChild(img);
+        }
+        document.getElementById("galleries").appendChild(clone);
+    })
+
+}
+
+
 class submission{
     constructor() {
         this.timestamp;
+        this.title;
         this.displayName;
         this.description;
         this.category;
         this.image;
+        this.id;
     }
 }
 
